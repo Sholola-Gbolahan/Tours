8dfee3e17ed1c1609542f6a2c8fd3b7c56e4a47f
Gbolahan Sholola <83152907+Sholola-Gbolahan@users.noreply.github.com>
2024-05-29 10:04:22 +0100

    Fetch Tours

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // Removing React.strictMode prevent double data import in console
+  // <React.StrictMode> 
     <App />
-  </React.StrictMode>,
-)
+  // </React.StrictMode>
+);
